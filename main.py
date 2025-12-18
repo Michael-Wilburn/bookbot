1462fb366bf7cdceee20c73172563008df1b7869
Michael Wilburn <michael-mw@hotmail.com>
2025-12-18 08:50:03 -0300

    Refactor

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from stats import get_num_words
+
 def get_book_test(filepath):
     with open(filepath) as f:
         file_contents = f.read()
-    return file_contents
-
+    return file_contents 
        
 def main():
-    print(get_book_test("./books/frankenstein.txt"))
+    str = get_book_test("./books/frankenstein.txt")
+    num_words = get_num_words(str)
+    print(f"Found {num_words} total words")
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(str):
+    list_words = str.split()
+    return len(list_words)
+   
